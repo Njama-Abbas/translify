56b36054c238cd874866d131aefc19304fe987eb
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-21 16:25:09 +0300

    *api changed sigup and sign in api to include user verification

--- app/backend/src/controllers/auth.controller.js
+++ app/backend/src/controllers/auth.controller.js
@@ -1,106 +1,115 @@
-const config = require("../config/auth.config");
-const db = require("../models");
-const User = db.user;
-const Role = db.role;
+const config = require("../config/auth.config"),
+  db = require("../models"),
+  phoneToken = require("generate-sms-verification-code"),
+  twilio = require("twilio"),
+  jwt = require("jsonwebtoken"),
+  bcrypt = require("bcrypt");
 
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const User = db.user,
+  Role = db.role,
+  sms = new twilio(config.T_SID, config.T_AUTH);
 
-exports.signup = (req, res) => {
-  const {
-    firstname,
-    lastname,
-    phoneno,
-    email,
-    password: userPassword,
-    role,
-  } = req.body;
+module.exports = {
+  signup: async (req, res) => {
+    const {
+      firstname,
+      lastname,
+      phoneno,
+      email,
+      password: userPassword,
+      role,
+    } = req.body;
 
-  Role.findOne(
-    {
+    const $role = Role.findOne({
       name: role,
-    },
-    (err, role) => {
-      if (err) {
-        res.status(500).json({ message: err });
-        return;
-      }
-      const user = new User({
-        firstname,
-        lastname,
-        phoneno,
-        email,
-        password: bcrypt.hashSync(userPassword, 10),
-        role: role._id,
+    });
+
+    //generate sms verification code
+    const generated_code = phoneToken(8, {
+      type: "number",
+    });
+
+    //create a new user
+    const new_user = await User.create({
+      firstname,
+      lastname,
+      phoneno,
+      email,
+      password: bcrypt.hashSync(userPassword, 10),
+      role: $role._id,
+      verification: {
+        code: generated_code,
+      },
+    });
+
+    const user = await new_user.save();
+
+    //send verification text message
+    sms.messages
+      .create({
+        body: `Tans-Code: ${user.verification.code} `,
+        to: "+254" + user.phoneno,
+        from: "+12027598622",
+      })
+      .then((message) => {
+        res.status(201).json({
+          userID: user._id,
+        });
       });
+  },
 
-      user.save((err, user) => {
-        if (err) {
-          res.status(500).json({ message: err });
-          return;
-        }
-        res.json({ message: "User was reqistered successfully" });
-      });
-    }
-  );
-};
+  signin: async (req, res) => {
+    const { email, password, role } = req.body;
 
-exports.signin = (req, res) => {
-  const { email, password, role } = req.body;
-
-  Role.findOne(
-    {
+    const $role = Role.findOne({
       name: role,
-    },
-    (err, role) => {
-      if (err) {
-        res.status(500).json({ message: err });
-        return;
-      }
+    });
 
-      User.findOne(
-        {
-          email,
-          role: role._id,
-        },
-        (err, user) => {
-          if (err) {
-            res.status(500).json({ message: err });
-            return;
-          }
+    const user = await User.findOne({
+      email,
+      role: $role._id,
+    });
 
-          if (!user) {
-            return res.status(404).json({
-              message: "User Not Found",
-            });
-          }
-
-          let valid = bcrypt.compareSync(password, user.password);
-
-          if (!valid) {
-            return res.status(401).json({
-              accessToken: null,
-              message: "Invalid Password",
-            });
-          }
-
-          let token = jwt.sign({ id: user.id }, config.secret, {
-            expiresIn: 86400,
-          });
-
-          const { _id: id, firstname, lastname, phoneno, email } = user;
-
-          res.status(200).json({
-            id,
-            firstname,
-            lastname,
-            email,
-            phoneno,
-            role: role.name,
-            accessToken: token,
-          });
-        }
-      );
+    //user is not in the database
+    if (!user) {
+      res.status(404).json({
+        message: "User Not Found",
+      });
+      return;
     }
-  );
+
+    //decrypt and validate password
+    let valid = bcrypt.compareSync(password, user.password);
+
+    if (!valid) {
+      res.status(401).json({
+        accessToken: null,
+        message: "Invalid Password",
+      });
+      return;
+    }
+
+    //Take care of a user signing in and is not verified
+    if (!user.verification.status) {
+      res.status(401).json({
+        message: "Not Authaurized",
+      });
+      return;
+    }
+
+    // if verified give the access token
+    let token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400,
+    });
+
+    res.status(200).json({
+      id: user._id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      phoneno: user.phoneno,
+      role: $role.name,
+      accessToken: token,
+    });
+  },
 };
