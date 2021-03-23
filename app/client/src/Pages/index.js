a8da219cc03abe51a0bef7a392d953b42194ac79
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-23 07:14:12 +0300

    *feat verification page

--- app/client/src/Pages/index.js
+++ app/client/src/Pages/index.js
@@ -3,7 +3,7 @@ export { default as SignUp } from "./SignUp";
 export { default as SignIn } from "./SignIn";
 export { default as Services } from "./Services";
 export { default as Discover } from "./Discover";
+export { default as Verification } from "./Verification";
 
 export { default as Client } from "./Client";
 export { default as Driver } from "./Driver";
-export { default as Admin } from "./Admin";
