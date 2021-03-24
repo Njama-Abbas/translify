7f58bb8ec11f74b2c82ddabd8fe0c08be818b98f
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-24 06:51:41 +0300

    *validate filter through phone numbers that start with 01 too

--- app/client/src/Resources/Patterns/validation.js
+++ app/client/src/Resources/Patterns/validation.js
@@ -1,7 +1,7 @@
 const ValidationPatterns = {
   name: /^[A-Za-z]+$/i,
   email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-  phoneno: /[0|+?254]7(\d){8}$/,
+  phoneno: /[0|+?254](\d){9}$/,
   password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
 };
 
