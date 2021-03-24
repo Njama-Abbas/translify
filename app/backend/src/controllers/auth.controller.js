2361d40c3ece6f79a0df4836161e068e685c4fca
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-24 06:47:18 +0300

    *api handling twilio errors and response

--- app/backend/src/controllers/auth.controller.js
+++ app/backend/src/controllers/auth.controller.js
@@ -6,8 +6,7 @@ const config = require("../config/auth.config"),
   bcrypt = require("bcrypt");
 
 const User = db.user,
-  Role = db.role,
-  sms = new twilio(config.T_SID, config.T_AUTH);
+  Role = db.role;
 
 module.exports = {
   signup: async (req, res) => {
@@ -30,19 +29,37 @@ module.exports = {
     });
 
     //create a new user
-    const new_user = await User.create({
-      firstname,
-      lastname,
-      phoneno,
-      email,
-      password: bcrypt.hashSync(userPassword, 10),
-      role: $role._id,
-      verification: {
-        code: generated_code,
-      },
-    });
+    let new_user;
+    try {
+      new_user = await User.create({
+        firstname,
+        lastname,
+        phoneno,
+        email,
+        password: bcrypt.hashSync(userPassword, 10),
+        role: $role._id,
+        verification: {
+          code: generated_code,
+        },
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: `Create User Error: ${e}`,
+      });
+      return;
+    }
 
     const user = await new_user.save();
+
+    let sms;
+    try {
+      sms = new twilio(config.TWILLIO_ACCOUNT_SID, config.TWILLIO_AUTH_TOKEN);
+    } catch (e) {
+      res.status(500).json({
+        message: `Twillio Authentication Error ${e}`,
+      });
+      return;
+    }
 
     //send verification text message
     let sendText;
@@ -55,15 +72,69 @@ module.exports = {
       });
     } catch (e) {
       res.status(500).json({
-        message: `Twillio Error ${e}`,
+        message: `Twillio Send Text Error ${e}`,
       });
       return;
     }
 
     res.status(201).json({
-      userID: user._id,
-      phone: user.phoneno,
+      UID: user._id,
+      phoneno: user.phoneno,
       sid: sendText.sid,
+    });
+  },
+
+  verify: async (req, res) => {
+    const { UID, v_code } = req.body;
+
+    const user = await User.findOne({
+      _id: UID,
+    });
+
+    if (!user) {
+      res.status(404).json({
+        message: "User nor found",
+      });
+      return;
+    }
+
+    if (user.verification.status) {
+      res.status(200).json({
+        message: "User Already verified",
+      });
+      return;
+    }
+
+    if (user.verification.code !== v_code) {
+      res.status(401).json({
+        message: "Wrong verification code",
+      });
+      return;
+    }
+
+    /**
+     * if code entered by user
+     * is equal to code in the database
+     * generate a json-web-token
+     * to sign in the user;
+     */
+
+    const $role = await Role.findOne({
+      _id: user.role,
+    });
+
+    let token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400,
+    });
+
+    res.status(200).json({
+      id: user._id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      phoneno: user.phoneno,
+      role: $role.name,
+      accessToken: token,
     });
   },
 
@@ -91,7 +162,7 @@ module.exports = {
     let valid = bcrypt.compareSync(password, user.password);
 
     if (!valid) {
-      res.status(401).json({
+      res.status(403).json({
         accessToken: null,
         message: "Invalid Password",
       });
@@ -102,6 +173,8 @@ module.exports = {
     if (!user.verification.status) {
       res.status(401).json({
         message: "Not Authaurized",
+        UID: user._id,
+        phoneno: user.phoneno,
       });
       return;
     }
