77b0b563e307b3ff6f729ba6f5669b72f4fd91a5
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-24 06:52:27 +0300

    *state added a users slice to hold user info before verification

--- app/client/src/State/Store.js
+++ app/client/src/State/Store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import ordersReducer from "./orders.slice";
 import navigationReducer from "./navigation.slice";
 import driversReducer from "./drivers.slice";
+import userReducer from "./user.slice";
 
 export default configureStore({
   reducer: {
     orders: ordersReducer,
     navigation: navigationReducer,
     drivers: driversReducer,
+    user: userReducer,
   },
 });
--- app/client/src/State/user.slice.js
+++ app/client/src/State/user.slice.js
@@ -1,9 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { AuthAPI } from "../Api";
+
+const initialState = {
+  user: {
+    UID: null,
+    phoneno: null,
+    verified: false,
+  },
+  status: "idle",
+  error: null,
+};
+
+const verifyUser = createAsyncThunk("user/verify", async (user) => {
+  const response = await AuthAPI.verify(user);
+  return response.data.v_status;
+});
 
 const UserSlice = createSlice({
   name: "user",
-  initialState: null,
-  reducers: {},
+  initialState,
+  reducers: {
+    userSet(state, action) {
+      const { UID, phoneno } = action.payload;
+      state.user.UID = UID;
+      state.user.phoneno = phoneno;
+    },
+    userVerified(state, action) {
+      state.user.verified = action.payload;
+    },
+  },
+  extraReducers: {
+    [verifyUser.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.payload;
+    },
+    [verifyUser.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [verifyUser.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.user.verified = action.payload;
+    },
+  },
 });
+
+export const { userSet, userVerified } = UserSlice.actions;
+
+export const selectUser = (state) => state.user.user;
+export const selectUserStatus = (state) => state.user.status;
 
 export default UserSlice.reducer;
