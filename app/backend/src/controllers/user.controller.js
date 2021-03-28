c0b5c630b267d50a95d4966c09591e2ebf1a210f
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-28 20:59:16 +0300

    *api moved user authentication to middlewares

--- app/backend/src/controllers/user.controller.js
+++ app/backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const User = db.user;
-const Role = db.role;
+const Driver = db.driver;
 
 module.exports = {
   allAccess: async (_req, res) => {
@@ -9,75 +9,10 @@ module.exports = {
     });
   },
 
-  driverBoard: async (req, res) => {
-    const driver_role = await Role.findOne({
-      name: "driver",
-    });
-
+  userDashBoard: async (req, res) => {
     const user = await User.findOne({
       _id: req.userId,
     });
-
-    if (!user) {
-      res.status(404).json({
-        message: "user not found",
-      });
-      return;
-    }
-
-    if (user.role !== driver_role._id) {
-      res.status(403).json({
-        message: "Require Driver Role",
-      });
-      return;
-    }
-
-    if (!user.verification.status) {
-      res.status(401).json({
-        message: "Not authorized user is unverified",
-      });
-      return;
-    }
-
-    res.status(200).json({
-      userId: user._id,
-      role: "driver",
-    });
-  },
-
-  clientBoard: async (req, res) => {
-    const client_role = await Role.findOne({
-      name: "client",
-    });
-
-    const user = await User.findOne({
-      _id: req.userId,
-    });
-
-    if (!user) {
-      res.status(404).json({
-        message: "user not found",
-      });
-      return;
-    }
-
-    if (user.role !== client_role._id) {
-      res.status(403).json({
-        message: "Require Client Role",
-      });
-      return;
-    }
-
-    if (!user.verification.status) {
-      res.status(401).json({
-        message: "Not authorized user is unverified",
-      });
-      return;
-    }
-
-    res.status(200).json({
-      userId: user._id,
-      role: "client",
-    });
+    res.status(200).json(user);
   },
 };
