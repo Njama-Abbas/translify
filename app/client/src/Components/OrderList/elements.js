f37122db59c837b9e662358194e8f222e8a4ede2
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-03 19:50:50 +0300

    *refact changed orders nav to have order control items and not buttons

--- app/client/src/Components/OrderList/elements.js
+++ app/client/src/Components/OrderList/elements.js
@@ -1,15 +1,9 @@
 import styled from "styled-components";
 import { Paper } from "@material-ui/core";
-import { Button } from "../../Resources/Styles/global";
 
 export const OrderCardContainer = styled.div`
   width: 100%;
-  margin-top: 64px;
   padding: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: center;
 `;
 
 export const OrderCardHeader = styled.p`
@@ -20,47 +14,82 @@ export const OrderCardHeader = styled.p`
   padding: 8px;
   width: 100%;
   text-align: center;
+  margin-top: 64px;
 `;
 
 export const OrdersNav = styled(Paper)`
   width: 100%;
-  display: flex;
-  justify-content: space-between;
-  background-color: rgba(0, 0, 0, 0.7) !important;
+  padding: 8px 4px;
+  background-color: rgba(0, 0, 0, 0) !important;
+  display: Grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+  row-gap: 8px;
 
-  @media screen and (max-width: 1200px) {
-    justify-content: center;
-    flex-wrap: wrap;
-  }
-
-  @media screen and (max-width: 960px) {
-    flex-wrap: nowrap;
+  @media (max-width: 600px) {
+    grid-template-columns: auto auto;
+    grid-template-rows: auto;
+    column-gap: 4px;
   }
 `;
 
-export const FilterButton = styled(Button)`
-  display: flex;
-  justify-content: space-between;
+export const FilterControlItem = styled.div`
+  background-color: ${({ selected }) =>
+    selected ? "rgba(0, 0,0, 0.7)" : "#3f51b5"};
+  display: grid;
+  cursor: pointer;
+  grid-template-columns: 25% 50% 25%;
+  justify-content: space-evenly;
+  column-gap: 10px;
   align-items: center;
-  margin: 5px;
+  height: 50px;
+  border-radius: 10px;
+  border-left: ${({ selected }) => (selected ? "1px solid blue" : "none")};
+  border-right: ${({ selected }) => (selected ? "1px solid blue" : "none")};
+
+  &:hover {
+    background-color: #0d1117 !important;
+  }
+  @media (max-width: 600px) {
+    grid-template-columns: auto auto auto;
+    justify-content: center;
+  }
 `;
 
 export const FilterText = styled.p`
-  @media screen and (max-width: 960px) {
-    display: none;
+  color: #fff;
+  font-size: 1.7em;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 1em;
   }
 `;
 
-export const Icon = styled.p`
+export const FilterIcon = styled.p`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  @media screen and (max-width: 960px) {
-    flex-grow: 1;
-  }
 `;
 
+export const FilterCount = styled.p`
+  background-color: #fff;
+  height: 40px;
+  color: #3f51b5;
+  border-radius: 50%;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  font-size: 1.5em;
+
+  @media (max-width: 600px) {
+    font-size: 1.2em;
+    width: 24px;
+    height: 25px;
+  }
+`;
 export const Notification = styled(Paper)`
   background-color: rgba(0, 0, 0, 0.7) !important;
   width: 100%;
