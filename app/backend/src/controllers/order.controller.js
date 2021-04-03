6d0edde97b70778dcc5ef92a3ed017caf0c20771
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-03 19:45:42 +0300

    *refactor +use of async functions +composed a map oders fucntion

--- app/backend/src/controllers/order.controller.js
+++ app/backend/src/controllers/order.controller.js
@@ -5,92 +5,222 @@ const DRIVER = db.driver;
 const ROLE = db.role;
 const USER = db.user;
 
-exports.creteOrder = (req, res) => {
-  const {
-    moveType,
-    clientId,
-    driverId,
-    pickup,
-    destination,
-    load,
-    charges,
-  } = req.body;
+module.exports = {
+  creteOrder: async (req, res) => {
+    const {
+      moveType,
+      clientId,
+      driverId,
+      pickup,
+      destination,
+      load,
+      charges,
+    } = req.body;
 
-  const newOrder = new ORDER({
-    moveType,
-    clientId,
-    driverId,
-    pickup,
-    destination,
-    load,
-    charges,
-    date: new Date(),
-  });
+    //add order to database
+    let new_order;
+    try {
+      new_order = await ORDER.create({
+        moveType,
+        clientId,
+        driverId,
+        pickup,
+        destination,
+        load,
+        charges,
+        date: new Date(),
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: err,
+      });
+      return;
+    }
+    const order = await new_order.save();
 
-  newOrder.save((err, order) => {
-    if (err) {
+    //Reserve driver until the order is completed
+    let designatedDriver;
+    try {
+      designatedDriver = await DRIVER.findByIdAndUpdate(driverId, {
+        reserved: true,
+      });
+    } catch (err) {
       res.status(500).json({
         message: err,
       });
       return;
     }
 
+    await designatedDriver.save();
+
+    const orders = [order];
+    const mappedOrders = MapOrders(orders);
+
     res.status(200).json({
-      order,
+      order: mappedOrders[0],
     });
-  });
-};
+  },
 
-exports.getAllOrdersByUserId = async (req, res) => {
-  const { userid } = req.headers;
+  getAllOrdersByUserId: async (req, res) => {
+    const { userid } = req.headers;
 
-  const user = await USER.findById(userid);
+    const user = await USER.findById(userid);
 
-  if (!user) {
-    res.status(404).json({
-      message: "User not found",
-    });
-    return;
-  }
-  const role = await ROLE.findById(user.role);
+    if (!user) {
+      res.status(404).json({
+        message: "User not found",
+      });
+      return;
+    }
+    const role = await ROLE.findById(user.role);
 
-  if (!role) {
-    res.status(404).json({
-      message: "We could not locate the supplied role",
-    });
-  }
+    if (!role) {
+      res.status(404).json({
+        message: "We could not locate the supplied role",
+      });
+    }
 
-  let orders = [];
+    let orders = [];
 
-  if (role.name === "client") {
-    orders = await ORDER.find({
-      clientId: userid,
-    });
-  } else if (role.name === "driver") {
+    if (role.name === "client") {
+      orders = await ORDER.find({
+        clientId: userid,
+      });
+    } else if (role.name === "driver") {
+      const driver = await DRIVER.findOne({
+        userId: userid,
+      });
+      if (!driver) {
+        res.status(404).json({
+          message: "Driver not found",
+        });
+        return;
+      }
+      orders = await ORDER.find({
+        driverId: driver._id,
+      });
+    }
+    if (!orders) {
+      res.status(404).json({
+        message: "orders not found",
+      });
+      return;
+    }
+    if (orders.length === 0) {
+      res.status(204).json(orders);
+      return;
+    }
+    const mappedOrders = await MapOrders(orders);
+    res.status(200).json(mappedOrders);
+  },
+
+  updateOrder: async (req, res) => {
+    const { OID, status, UID } = req.body;
+
+    // updating the order
+    let order;
+    try {
+      order = await ORDER.findByIdAndUpdate(OID, {
+        status,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: err,
+      });
+      return;
+    }
+
+    //if null result
+    if (!order) {
+      res.status(404).json({
+        message: "order not found",
+      });
+      return;
+    }
+
+    //successfull >
+    //1. successfull
+    const user = await USER.findById(UID);
+
+    if (!user) {
+      res.status(404).json({
+        message: "User not found",
+      });
+      return;
+    }
+
+    const role = await ROLE.findById(user.role);
+
+    if (!role) {
+      res.status(404).json({
+        message: "We could not locate the supplied role",
+      });
+    }
+
     const driver = await DRIVER.findOne({
-      userId: userid,
+      _id: order.driverId,
     });
+
     if (!driver) {
       res.status(404).json({
         message: "Driver not found",
       });
       return;
     }
-    orders = await ORDER.find({
-      driverId: driver._id,
-    });
-  }
-  if (!orders) {
-    res.status(404).json({
-      message: "orders not found",
-    });
-    return;
-  }
-  if (orders.length === 0) {
-    res.status(204).json(orders);
-    return;
-  }
 
+    //unreserve driver
+    if (status == "cancelled" || status == "successfull") {
+      let designatedDriver;
+      try {
+        designatedDriver = await DRIVER.findByIdAndUpdate(driver._id, {
+          reserved: false,
+        });
+      } catch (err) {
+        res.status(500).json({
+          message: err,
+        });
+        return;
+      }
+      await designatedDriver.save();
+    }
+
+    let orders = [];
+
+    if (role.name === "client") {
+      orders = await ORDER.find({
+        clientId: UID,
+      });
+    } else if (role.name === "driver") {
+      orders = await ORDER.find({
+        driverId: driver._id,
+      });
+    }
+
+    //no orders available for user ids
+    if (!orders) {
+      res.status(404).json({
+        message: "orders not found",
+      });
+      return;
+    }
+
+    //order collection is empty
+    if (orders.length === 0) {
+      res.status(204).json(orders);
+      return;
+    }
+
+    const mappedOrders = await MapOrders(orders);
+
+    res.status(200).json(mappedOrders);
+  },
+};
+/**
+ *
+ * @param {[]} orders
+ * @returns {[]} an array of orders mapped eith driver and client names
+ */
+async function MapOrders(orders) {
   const mappedOrders = await Promise.all(
     orders.map(async (order) => {
       const {
@@ -136,44 +266,5 @@ exports.getAllOrdersByUserId = async (req, res) => {
       };
     })
   );
-  res.status(200).json(mappedOrders);
-};
-
-exports.updateOrder = (req, res) => {
-  const { orderId: _id, status } = req.body;
-
-  ORDER.findOne(
-    {
-      _id,
-    },
-    (err, order) => {
-      if (err) {
-        res.status(500).json({
-          message: err,
-        });
-        return;
-      }
-
-      if (!order) {
-        res.status(404).json({
-          message: "order not found",
-        });
-        return;
-      }
-
-      order.status = status;
-
-      order.save((err, order) => {
-        if (err) {
-          res.status(500).json({
-            message: err,
-          });
-          return;
-        }
-        res.status(200).json({
-          message: "Order Updated Successfully",
-        });
-      });
-    }
-  );
-};
+  return mappedOrders;
+}
