af5598b67e6a713a7da3f4e9d26df69e9285443e
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-08 18:10:15 +0300

    *style +image background container for how it works page

--- app/client/src/Pages/HomePage/index.js
+++ app/client/src/Pages/HomePage/index.js
@@ -1,15 +1,17 @@
 import React, { Fragment } from "react";
 import { Footer, InfoSection, Navbar, HowItWorks } from "../../Components";
 import { HeroInfo } from "../../Resources/Data/heroInfo";
+import { ImageBgContainer } from "../../Resources/Styles/global";
 import { Services } from "../index";
-
 export default function Home() {
   return (
     <Fragment>
       <Navbar />
       <InfoSection {...HeroInfo} />
       <Services />
-      <HowItWorks />
+      <ImageBgContainer>
+        <HowItWorks />
+      </ImageBgContainer>
       <Footer />
     </Fragment>
   );
