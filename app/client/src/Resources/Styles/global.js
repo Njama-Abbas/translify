19de18601ac844127c6df575b39ea079e8511120
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-12 06:13:54 +0300

    *style +no image resize on small width feature

--- app/client/src/Resources/Styles/global.js
+++ app/client/src/Resources/Styles/global.js
@@ -75,7 +75,7 @@ export const Button = styled.button`
   }
 
   @media screen and (max-width: 960px) {
-    width: 100%;
+    width: ${({ nochangeonsmall }) => (nochangeonsmall ? "initial" : "100%")};
   }
 `;
 
