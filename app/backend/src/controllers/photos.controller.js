3fe02519f1c400cd3dc2b7a63fa9a6bb828ea2d5
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-19 18:50:22 +0300

    *fixed + profile photo upload success ^uppercased variable names for db documents

--- app/backend/src/controllers/photos.controller.js
+++ app/backend/src/controllers/photos.controller.js
@@ -1,43 +1,33 @@
 const db = require("../models");
-const Photo = db.photo;
+const PHOTO = db.photo;
 
 exports.uploadPhoto = (req, res) => {
-  const { userId, photo } = req.body;
+  const { userId, photo_name } = req.body;
   const file = req.file.buffer;
 
-  console.log({
-    file,
-    photo,
+  const profilePhoto = new PHOTO({
     userId,
+    photo: file,
   });
 
-  // const ProfilePhoto = new Photo({
-  //   userId,
-  //   photo_name,
-  //   photo: file,
-  // });
-
-  // ProfilePhoto.save((err, photo) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(500).json({
-  //       message: err,
-  //     });
-  //     return;
-  //   }
-  //   res.status(200).json({
-  //     photo_id: photo._id,
-  //   });
-  // });
-  res.status(200).json({
-    ds: "dsau",
+  profilePhoto.save((err, photo) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        message: err,
+      });
+      return;
+    }
+    res.status(200).json({
+      photo_id: photo._id,
+    });
   });
 };
 
 exports.getPhotoById = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await Photo.findById(id);
+    const result = await PHOTO.findById(id);
     res.type(".png").send(result.photo);
   } catch (error) {
     res.status(400).json({
@@ -49,7 +39,7 @@ exports.getPhotoById = async (req, res) => {
 exports.getUserProfilePhotoID = async (req, res) => {
   const { userid: userId } = req.headers;
 
-  Photo.findOne(
+  PHOTO.findOne(
     {
       userId,
     },
