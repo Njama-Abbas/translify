6a41e398476b71604f916741a95c8c5f74103ab2
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-21 06:32:19 +0300

    *feat added the following functionalities +uplading image +changing image +lading component +toast notifications

--- app/client/src/Components/ProfilePhoto/index.js
+++ app/client/src/Components/ProfilePhoto/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-
+import React, { useState, useEffect } from "react";
+import { useToasts } from "react-toast-notifications";
 import { AuthAPI, PhotoAPI } from "../../Api";
 
 import {
@@ -11,39 +10,88 @@ import {
   FileInput,
   FormPaper,
   ProfileControls,
+  ProfileLoaderContainer,
 } from "./elements";
+import LoadingComponent from "../LoadingComponent";
 
 import imagePlaceHolder from "../../Resources/Images/undraw_profile_pic.svg";
 import { IconContext } from "react-icons/lib";
 import { Dialog, DialogContent, Slide } from "@material-ui/core";
 import { Button } from "../../Resources/Styles/global";
 import { FaEdit } from "react-icons/fa";
-import { profilePicSet } from "../../State/user.slice";
 
 export default function FileUploadComponent() {
-  const user = AuthAPI.getCurrentUser();
+  const [profilePhotoID, setProfilePhotoID] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <Container>
-      <Wrapper>
-        <ProfileImage
-          src={
-            user.profilePhotoId
-              ? `http://localhost:801/api/photos/${user.profilePhotoId}`
-              : imagePlaceHolder
+  const user = AuthAPI.getCurrentUser();
+  const { addToast } = useToasts();
+
+  useEffect(() => {
+    PhotoAPI.getProfileID(user.id).then(
+      (response) => {
+        setProfilePhotoID(response.data.photo_id);
+      },
+      (error) => {
+        if (error.response) {
+          if (error.response.status !== 404) {
+            addToast(
+              `An error occured while fetching
+      profile picture please try angain letter`,
+              {
+                appearance: "error",
+              }
+            );
           }
-        />
-        <EditImageDialog user={user} />
-      </Wrapper>
-    </Container>
-  );
+        }
+      }
+    );
+  }, [addToast, user.id]);
+
+  const handleProfilePicChange = (id) => {
+    setProfilePhotoID(id);
+  };
+
+  const setLoading = (state) => {
+    setIsLoading(state);
+  };
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <ProfileLoaderContainer>
+        <LoadingComponent small message="Updating your details.." />;
+      </ProfileLoaderContainer>
+    );
+  } else {
+    content = (
+      <Container>
+        <Wrapper>
+          <ProfileImage
+            src={
+              profilePhotoID && !isLoading
+                ? `http://localhost:801/api/photos/${profilePhotoID}`
+                : imagePlaceHolder
+            }
+          />
+          <EditImageDialog
+            user={user}
+            updateProfilePic={handleProfilePicChange}
+            setLoading={setLoading}
+          />
+        </Wrapper>
+      </Container>
+    );
+  }
+  return content;
 }
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-function EditImageDialog({ user }) {
+function EditImageDialog({ user, updateProfilePic, setLoading }) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -73,7 +121,11 @@ function EditImageDialog({ user }) {
           aria-describedby="edit-profile-dialog-slide-description"
         >
           <DialogContent>
-            <EditImageForm user={user} />
+            <EditImageForm
+              user={user}
+              updateProfilePic={updateProfilePic}
+              setLoading={setLoading}
+            />
           </DialogContent>
         </Dialog>
       </div>
@@ -81,9 +133,9 @@ function EditImageDialog({ user }) {
   );
 }
 
-function EditImageForm({ user }) {
+function EditImageForm({ user, updateProfilePic, setLoading }) {
   const [profileImg, setProfileImg] = useState("");
-  const dispatch = useDispatch();
+  const { addToast } = useToasts();
 
   const onFileChange = (e) => {
     setProfileImg(e.target.files[0]);
@@ -91,7 +143,7 @@ function EditImageForm({ user }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    setLoading(true);
     const formData = new FormData();
     formData.append("userId", user.id);
     formData.append("photo_name", profileImg.name);
@@ -99,11 +151,27 @@ function EditImageForm({ user }) {
 
     PhotoAPI.upload(formData).then(
       (response) => {
-        console.log(response);
-        dispatch(profilePicSet(response.data.photo_id));
+        updateProfilePic(response.data.photo_id);
+        addToast(
+          `Profile picture updated It might take
+          a while To reflect the changes.
+            `,
+          {
+            appearance: "error",
+          }
+        );
+        setLoading(false);
       },
       (error) => {
         console.log(error);
+        addToast(
+          `An error occured, cannot change profile picture,
+            please try angain letter`,
+          {
+            appearance: "error",
+          }
+        );
+        setLoading(false);
       }
     );
   };
