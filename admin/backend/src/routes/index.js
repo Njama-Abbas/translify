2a0551ccbc4318b817e213b663ef7947efb39eb3
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-25 19:18:37 +0300

    *API +orders Route

--- admin/backend/src/routes/index.js
+++ admin/backend/src/routes/index.js
@@ -2,7 +2,10 @@ const client = require("./client.routes");
 
 const driver = require("./driver.routes");
 
+const order = require("./orders.routes");
+
 module.exports = {
   driver,
   client,
+  order,
 };
