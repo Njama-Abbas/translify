cc03f5d3727fb9e78c000c09e3f158bfc14ed180
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-25 19:18:15 +0300

    *API +approve controller for put requests

--- admin/backend/src/routes/driver.routes.js
+++ admin/backend/src/routes/driver.routes.js
@@ -1,23 +1,26 @@
 const driverController = require("../controllers/driver.controller");
-const {
-  driver_sift,
-  user_sift
-} = require("../middlewares");
+const { driver_sift, user_sift } = require("../middlewares");
 
 const Router = require("express").Router();
 
-Router.post("/create", [
-  user_sift.checkDuplicateEmail, user_sift.checkDuplicatePhoneNo, user_sift.checkRolesExisted,
-  driver_sift.checkDuplicateUserId,
-  driver_sift.checkDuplicateDlno,
-  driver_sift.checkDuplicateTruckNo,
-], driverController.create);
+Router.post(
+  "/create",
+  [
+    user_sift.checkDuplicateEmail,
+    user_sift.checkDuplicatePhoneNo,
+    user_sift.checkRolesExisted,
+    driver_sift.checkDuplicateUserId,
+    driver_sift.checkDuplicateDlno,
+    driver_sift.checkDuplicateTruckNo,
+  ],
+  driverController.create
+);
 
 Router.get("/", driverController.fetch);
 
 Router.get("/:id", driverController.get);
 
-Router.put("/:id", driverController.update);
+Router.put("/:id", driverController.approve);
 
 Router.delete("/:id", driverController.delete);
 
