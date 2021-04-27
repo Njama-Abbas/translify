8e9422ca8c0c1d6f34912289bed8822ff7c8c333
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-27 06:22:22 +0300

    *feat +loading component +managing state for child components

--- app/client/src/Pages/Profile/index.js
+++ app/client/src/Pages/Profile/index.js
@@ -4,6 +4,7 @@ import {
   PersonalDetails,
   PersonalActions,
   ProfilePhoto,
+  LoadingComponent,
 } from "../../Components";
 import { AuthAPI } from "../../Api";
 import { ImageBgContainer } from "../../Resources/Styles/global";
@@ -14,6 +15,11 @@ export default function Profile() {
   const [redirect, setRedirect] = useState(null);
   const [userReady, setUserReady] = useState(null);
   const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLoading = (state) => {
+    setIsLoading(state);
+  };
 
   useEffect(() => {
     const currentUser = AuthAPI.getCurrentUser();
@@ -25,25 +31,29 @@ export default function Profile() {
     setUserReady(true);
   }, []);
 
+  let content;
   if (redirect) {
-    return <Redirect to={redirect} />;
+    content = <Redirect to={redirect} />;
+  } else if (isLoading) {
+    content = <LoadingComponent message={"updating details..."} />;
+  } else {
+    content = (
+      <div className="container">
+        {userReady ? (
+          <ImageBgContainer>
+            <GreetingLine>
+              {/**to be determined by current time */}
+              Greetings&nbsp;&nbsp;{currentUser.firstname}
+            </GreetingLine>
+            <ProfilePhoto />
+            <Grid container spacing={2} justify="center">
+              <PersonalDetails user={currentUser} setLoading={handleLoading} />
+              <PersonalActions user={currentUser} />
+            </Grid>
+          </ImageBgContainer>
+        ) : null}
+      </div>
+    );
   }
-
-  return (
-    <div className="container">
-      {userReady ? (
-        <ImageBgContainer>
-          <GreetingLine>
-            {/**to be determined by current time */}
-            Greetings&nbsp;&nbsp;{currentUser.firstname}
-          </GreetingLine>
-          <ProfilePhoto />
-          <Grid container spacing={2} justify="center">
-            <PersonalDetails user={currentUser} />
-            <PersonalActions user={currentUser} />
-          </Grid>
-        </ImageBgContainer>
-      ) : null}
-    </div>
-  );
+  return content;
 }
