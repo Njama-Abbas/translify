0d471b992d3973ba449b88b9aede632952f3788c
Baraka Mulumia <barakamulumia@gmail.com>
2021-04-27 06:16:37 +0300

    *api +routes for 1.update info 2.change-pwd-code 3. change pwd

--- app/backend/src/routes/auth.routes.js
+++ app/backend/src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 const Router = require("express").Router();
 
-const {
-  user_sift
-} = require("../middlewares");
+const { user_sift } = require("../middlewares");
 const AuthController = require("../controllers/auth.controller");
 
 Router.post(
   "/signup",
-  [user_sift.checkDuplicateEmail, user_sift.checkDuplicatePhoneNo, user_sift.checkRolesExisted],
+  [
+    user_sift.checkDuplicateEmail,
+    user_sift.checkDuplicatePhoneNo,
+    user_sift.checkRolesExisted,
+  ],
   AuthController.signup
 );
 
@@ -16,5 +18,14 @@ Router.post("/signin", AuthController.signin);
 Router.post("/verify-account", AuthController.verify);
 
 Router.post("/resend-vcode", AuthController.resendVerificationCode);
+
+Router.put("/update-info", AuthController.updateInfo);
+
+Router.post(
+  "/change-password-code",
+  AuthController.sendChangePasswordVerificationCode
+);
+
+Router.post("/change-password", AuthController.changePassword);
 
 module.exports = Router;
