b9fd69649457fad9733eda867eb07ed877dc815e
Baraka Mulumia <barakamulumia@gmail.com>
2021-05-03 22:11:29 +0300

    *style +light bg color

--- app/client/src/Pages/Verification/elements.js
+++ app/client/src/Pages/Verification/elements.js
@@ -13,12 +13,13 @@ export const VerificationWrapper = styled(Grid)`
   border-radius: 5px;
   text-align: center;
   padding-bottom: 10px;
-  background-color: transparent;
+  background-color: rgba(253, 233, 233, 0.849);
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 
   @media (max-width: 600px) {
     max-height: 80vh;
+    width: 100%;
   }
 `;
 
