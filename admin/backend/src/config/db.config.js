fe43ee27789ed87b4e5c040ceb376e4b5eac479c
Baraka Mulumia <barakamulumia@gmail.com>
2021-05-09 20:37:00 +0300

    *fixed +db config object --removed extra semi colon

--- admin/backend/src/config/db.config.js
+++ admin/backend/src/config/db.config.js
@@ -3,8 +3,8 @@ module.exports = {
   user: process.env.DATABASE_USER,
   db_name: process.env.DATABASE_NAME,
   local: {
-  HOST: "localhost",
-  PORT: 27017,
-  DB: "translify_DB",
-};
+    HOST: "localhost",
+    PORT: 27017,
+    DB: "translify_DB",
+  },
 };
