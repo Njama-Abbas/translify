436f217d159cdc803e027ad948057ec40e095e85
Baraka Mulumia <barakamulumia@gmail.com>
2021-05-14 19:31:36 +0300

    *cleanup -redundant comments

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import ScrollToTop from "./Components/ScrollToTop";
 import GlobalStyle from "./Resources/Styles/global";
-// import FileUploadComponent from "./Components/ProfilePhoto";
 import { ToastProvider } from "react-toast-notifications";
 
 import {
@@ -25,25 +24,20 @@ export default function App() {
         <Route exact path={["/", "/home"]} component={Home} />
         <Route exact path="/discover" component={Discover} />
         <Route exact path="/verify-account" component={Verification} />
-
         <Route exact path="/driver/sign-up">
           <SignUp route="driver" />
         </Route>
         <Route exact path="/driver/sign-in">
           <SignIn route="driver" />
         </Route>
-
         <Route exact path="/client/sign-up">
           <SignUp route="client" />
         </Route>
         <Route exact path="/client/sign-in">
           <SignIn route="client" />
         </Route>
-
         <Route exact path="/forgot-password" component={ForgotPassword} />
         <Route exact path="/reset-password" component={ResetPassword} />
-
-        {/* <Route exact path="/test" component={FileUploadComponent} />**/}
         <Route path="/client" component={Client} />
         <Route path="/driver" component={Driver} />
       </Switch>
